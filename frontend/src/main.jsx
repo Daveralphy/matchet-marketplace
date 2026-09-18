cd2daa45c0d768a95f6a3569ad0fe888d3bfb219
Jorge Menjivar <menjivar.jorge.93@gmail.com>
2026-09-17 19:30:08 -0600

    Created Login page, UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import "./styles/globals.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
